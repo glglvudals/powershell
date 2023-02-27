6a9c6ebaeb56a72586eb23cefeac8c3d9e1dba08
glglvudals <100690293+glglvudals@users.noreply.github.com>
2023-02-26 19:28:14 -1200

    Add files via upload

---
+++ Add_dns_record.ps1
@@ -0,0 +1,4 @@
+﻿Add-DnsServerResourceRecordA -Name KH-ESXi-01 -IPv4Address 10.10.1.11 -ZoneName wish.kh
+Add-DnsServerResourceRecordA -Name KH-ESXi-02 -IPv4Address 10.10.1.12 -ZoneName wish.kh
+Add-DnsServerResourceRecordA -Name KH-ESXi-03 -IPv4Address 10.10.1.13 -ZoneName wish.kh
+Add-DnsServerResourceRecordA -Name KH-VCSA -IPv4Address 10.10.1.20 -ZoneName wish.kh
