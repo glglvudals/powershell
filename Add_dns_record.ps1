1a52f5e0f71a087949c650b98bcf9ba4ab614813
glglvudals <100690293+glglvudals@users.noreply.github.com>
2023-02-27 14:59:43 -1200

    Update Add_dns_record.ps1

--- Add_dns_record.ps1
+++ Add_dns_record.ps1
@@ -2,6 +2,8 @@ Add-DnsServerResourceRecordA -Name KH-ESXi-01 -IPv4Address 10.10.1.11 -ZoneName 
 Add-DnsServerResourceRecordA -Name KH-ESXi-02 -IPv4Address 10.10.1.12 -ZoneName wish.kh
 Add-DnsServerResourceRecordA -Name KH-ESXi-03 -IPv4Address 10.10.1.13 -ZoneName wish.kh
 Add-DnsServerResourceRecordA -Name KH-VCSA -IPv4Address 10.10.1.20 -ZoneName wish.kh
+Add-DnsServerPrimaryZone -name ps.kh -ZoneFile ps.kh.dns
+Add-DnsServerResourceRecordA -Name KH-VCSA -IPv4Address 10.10.1.20 -ZoneName ps.kh
 Add-DnsServerPrimaryZone -NetworkId 10.10.1.0/24 -ZoneFile 1.10.10.in-addr.arpa.dns
 Add-DnsServerResourceRecordPtr -Name 11 -ZoneName 1.10.10.in-addr.arpa -PtrDomainName KH-ESXi-01.wish.kh
 Add-DnsServerResourceRecordPtr -Name 12 -ZoneName 1.10.10.in-addr.arpa -PtrDomainName KH-ESXi-02.wish.kh
