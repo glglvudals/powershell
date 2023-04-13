96b0721f3c67f43a2de5e3e8120869f958d4561a
glglvudals <100690293+glglvudals@users.noreply.github.com>
2023-04-13 09:48:05 +0900

    Add files via upload

---
+++ chrome_download&defaultbrowser.ps1
@@ -0,0 +1,10 @@
+﻿Invoke-WebRequest -Uri "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B8A1035B0-3C0C-E957-17D9-ECBDFEBB564D%7D%26lang%3Dko%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe" -OutFile "$env:USERPROFILE\Downloads\ChromeSetup.exe"
+Start-Process -FilePath "$env:USERPROFILE\Downloads\ChromeSetup.exe"
+REG ADD "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice" /v "ProgId" /d "ChromeHTML" /f
+REG ADD "HKEY_CURRENT_USER\SOFTWARE\Classes\.htm" /ve /d "ChromeHTML" /f
+REG ADD "HKEY_CURRENT_USER\SOFTWARE\Classes\.html" /ve /d "ChromeHTML" /f
+REG ADD "HKEY_CURRENT_USER\SOFTWARE\Classes\.shtml" /ve /d "ChromeHTML" /f
+REG ADD "HKEY_CURRENT_USER\SOFTWARE\Classes\.xht" /ve /d "ChromeHTML" /f
+REG ADD "HKEY_CURRENT_USER\SOFTWARE\Classes\.xhtml" /ve /d "ChromeHTML" /f
+
+
---
+++ download_ADK.ps1
@@ -0,0 +1,6 @@
+Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=2165884 -OutFile "$env:USERPROFILE\Downloads\ADK_win11.exe"
+# win2022: https://go.microsoft.com/fwlink/?linkid=2162950
+# win10_ver2004: https://go.microsoft.com/fwlink/?linkid=2120254
+# win10_ver1903 : https://go.microsoft.com/fwlink/?linkid=2026036
+# win10_ver1809 : https://go.microsoft.com/fwlink/?linkid=2026036
+
